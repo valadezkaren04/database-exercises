a9e933dd90f360cc5c7ccaedf709f5b6151c3e92
Karen Valadez <valadez.karen04@gmail.com>
2021-06-18 08:54:27 -0500

    exercise completed

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,5 +2,13 @@ USE codeup_test_db;
 
  DROP TABLE IF EXISTS albums;
 
-
+CREATE TABLE albums (
+   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+   artist VarCHAR(50),
+   name  VARCHAR(100),
+   release_date INT,
+   sales DECIMAL(15, 2),
+   genre VARCHAR(50),
+   PRIMARY KEY (id)
+);
 
