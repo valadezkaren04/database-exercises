57920a301ae91431bccccd8c31f7155e9ff9b596
Karen Valadez <valadez.karen04@gmail.com>
2021-06-21 10:33:26 -0500

    commit

--- where-clauses.sql
+++ where-clauses.sql
@@ -9,3 +9,14 @@ WHERE emp_no BETWEEN 10026 AND 10082;
 
 SELECT emp_no, first_name, last_name FROM employees
 WHERE last_name IN ('Herber', 'Dredge', 'Lipner', 'Baek');
+
+SELECT emp_no, first_name, last_name FROM employees
+WHERE emp_no < 10026;
+
+SELECT emp_no, first_name, last_name FROM employees
+WHERE emp_no < 20000
+    AND last_name IN ('Herber', 'Baek')
+   OR first_name = 'Shridhar';
+
+# this is the old way oof doing it
+SELECT * FROM salaries WHERE NOT salary BETWEEN 50000 AND 100000;
