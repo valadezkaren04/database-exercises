4c8d451217a0b74d769e70a1fa048052731b3eab
Karen Valadez <valadez.karen04@gmail.com>
2021-06-28 10:26:56 -0500

    progress made..

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,4 +1,5 @@
 USE employees;
 
-SELECT first_name, last_name, emp_no FROM employees
+SELECT * FROM employees WHERE hire_date IN (SELECT hire_date FROM employees WHERE emp_no = '101010');
 
+SELECT first_name, last_name FROM employees WHERE
