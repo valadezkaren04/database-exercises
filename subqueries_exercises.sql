6e1b5e4a30903b4a56189cafa1e36d3668a6ece6
Karen Valadez <valadez.karen04@gmail.com>
2021-06-28 10:32:43 -0500

    exercise complete

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -2,4 +2,9 @@ USE employees;
 
 SELECT * FROM employees WHERE hire_date IN (SELECT hire_date FROM employees WHERE emp_no = '101010');
 
-SELECT first_name, last_name FROM employees WHERE
+SELECT title FROM titles WHERE emp_no IN (SELECT emp_no FROM employees WHERE first_name = 'Aamod');
+
+SELECT DISTINCT title FROM titles WHERE emp_no IN (SELECT emp_no FROM employees WHERE first_name = 'Aamod');
+
+SELECT first_name, last_name FROM employees WHERE emp_no
+IN (SELECT emp_no FROM dept_manager WHERE to_date LIKE '9999%') AND gender = 'F';
